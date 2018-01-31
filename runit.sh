fe371c7706a10983ef4ccb701d339e0051da65c7
robertej17 <robertej17@gmail.com>
2018-01-31 14:55:03 -0500

    start commit

--- runit.sh
+++ runit.sh
@@ -3,4 +3,64 @@ root -l plotroot.C
 -q
 
 
+sshpass -p 'l3pass' scp /home/bobby/wavedump-3.8.1/src/newwavedumpfile robertej@dmtpc00.lns.mit.edu:/home/robertej/
+
+
+
+
+Reading at 0.04 MB/s (Trg Rate: 21.98 Hz)
+2053
+2051
+2053
+2052
+2052
+2054
+2053
+2051
+2051
+2053
+2052
+2055
+2053
+2053
+2052
+2053
+Reading at 0.03 MB/s (Trg Rate: 15.98 Hz)
+2054
+2054
+2051
+2052
+2052
+2052
+2051
+2052
+2051
+2055
+2053
+2053
+2053
+2054
+2053
+2052
+2054
+Reading at 0.03 MB/s (Trg Rate: 16.98 Hz)
+2053
+2052
+2051
+2053
+2051
+2053
+2053
+2052
+2054
+2054
+2052
+2053
+2053
+2054
+2051
+2052
+2051
+2051
+Reading at 0.04 MB/s (Trg Rate: 17.98 Hz)
 
